270af1b2307276d53af4b0dcf7058f9fd40466b3
beaudavenport <davenportbeau@gmail.com>
2013-12-06 12:47:28 -0600

    First STL Tap Report Upload

---
+++ STLTapReport/STLTapReport/Controllers/STLTapReportController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace STLTapReport.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /STLTapReport/
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello World!";
+            return View();
+        }
+
+        public ActionResult Beers()
+        {
+            return View();
+        }
+
+        public ActionResult Styles()
+        {
+            return View();
+        }
+    }
+}
---
+++ STLTapReport/STLTapReport/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="STLTapReport.MvcApplication" Language="C#" %>
